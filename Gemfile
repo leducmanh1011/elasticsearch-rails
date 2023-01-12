24d40ecdfbd9c00137be002f214b5887a00563f5
ManhLD <leducmanh101198@gmail.com>
2023-01-12 16:55:43 +0700

    Seed data

--- Gemfile
+++ Gemfile
@@ -42,6 +42,9 @@ gem 'bootstrap', '~> 5.1.3'
 # Implement elasticsearch
 gem "searchkick"
 
+# Fake data
+gem 'faker'
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+50.times do |n|
+  artist = Artist.create! name: Faker::Name.unique.name, gender: rand(0..1)
+  artist.albums.create! title: Faker::Music.album
+end
+
+15.times do 
+  Genre.find_or_create_by! name: Faker::Music.genre
+end
+
+genre_ids = Genre.ids
+
+Album.all.each do |a|
+  Track.create! name: Faker::Name.unique.name, album_id: a.id, genre_id: genre_ids.sample,
+    composer: Faker::Music.band, seconds: Array(3173..5893).sample, realease_date: rand(10.years.ago.to_date..Date.current - 1.month)
+end
