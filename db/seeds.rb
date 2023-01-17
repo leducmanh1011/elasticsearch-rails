a0431474cee2aff732ebe1a2970c89c20b2681fd
ManhLD <leducmanh101198@gmail.com>
2023-01-17 09:55:27 +0700

    faker location jp

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,22 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+Faker::Config.locale = :ja
 
-50.times do |n|
-  artist = Artist.create! name: Faker::Name.unique.name, gender: rand(0..1)
-  artist.albums.create! title: Faker::Music.album
-end
+Chewy.strategy(:atomic) do
+  50.times do |n|
+    artist = Artist.create! name: Faker::Name.unique.name, gender: rand(0..1)
+    artist.albums.create! title: Faker::Music.album
+  end
 
-15.times do 
-  Genre.find_or_create_by! name: Faker::Music.genre
-end
+  15.times do 
+    Genre.find_or_create_by! name: Faker::Music.genre
+  end
 
-genre_ids = Genre.ids
+  genre_ids = Genre.ids
 
-Album.all.each do |a|
-  Track.create! name: Faker::Name.unique.name, album_id: a.id, genre_id: genre_ids.sample,
-    composer: Faker::Music.band, seconds: Array(3173..5893).sample, realease_date: rand(10.years.ago.to_date..Date.current - 1.month)
+  Album.all.each do |a|
+    Track.create! name: Faker::Name.unique.name, album_id: a.id, genre_id: genre_ids.sample,
+      composer: Faker::Music.band, seconds: Array(3173..5893).sample, realease_date: rand(10.years.ago.to_date..Date.current - 1.month)
+  end
 end
